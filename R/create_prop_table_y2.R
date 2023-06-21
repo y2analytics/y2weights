361931ad5f3fb362beae0fc2a2063aa70d7bf022
Canyen Heimuli <canyenheimuli@gmail.com>
2023-06-21 10:29:03 -0400

    Updates for initial release

--- R/create_prop_table_y2.R
+++ R/create_prop_table_y2.R
@@ -5,10 +5,9 @@
 #' Use create_prop_table_y2() to create nicely formatted population parameter tables to be used for weighting data. To avoid typos, create_prop_table_y2() will automatically create an object in your R environment using the provided variable to create the name. This function is designed to work within the y2weights framework.
 #'
 #' @keywords freqs weights population parameters
-#' @param dataset A dataframe to be weighted
-#' @param variable The variable for which you will be supplying population parameters
-#' @param population_params A named vector containing population parameters
-#' @param add_missing_row DEFAULT = FALSE, Set to TRUE to add a row to the prop table that takes into account "MISSING" values in the variable for which you are supplying population parameters
+#' @param dataset A dataframe with variables to be used in weighting.
+#' @param variable The variable for which you will be supplying population parameters.
+#' @param population_params A named vector containing population parameters.
 #' @export
 #' @importFrom rlang .data
 #' @importFrom data.table :=
@@ -27,68 +26,124 @@
 create_prop_table_y2 <- function(
     dataset,
     variable,
-    population_params, 
-    add_missing_row = FALSE
+    population_params
 ){
   
-  ### Get the variable as a string
+  ## Get the variable as a string
   variable_string <- 
     deparse(
       substitute(
         variable
-        )
       )
-  
-  ### Make pps name as string
-  pps_string <- 
-    stringr::str_c(
-      variable_string,
-      '_prop_table'
     )
   
-  ### make prop table without the row for MISSING levels
-  if(add_missing_row == TRUE){
+  ## Error if population_params is unnamed
+  
+  if (!rlang::is_named(population_params)){
     
+    stop('Population params vector is unnamed; please add levels and proportions')
+    
+  }
+  
+  ## Get var_levels for control flow
+  var_levels <- dataset %>% 
+    dplyr::count({{ variable }}) %>% 
+    dplyr::pull({{ variable }})
+  
+  ## Make the prop table when there are missing levels
+  if ('MISSING' %in% var_levels) {
+    
+    # Throws error if variable still has missing values
+    if (NA %in% var_levels) {
+      
+      stop(
+        stringr::str_c(
+          'variable ',
+          variable_string,
+          ' contains NA values; please set to "MISSING"'
+        )
+      )
+      
+    }
+    
+    # Table
     prop_table <- 
       tibble::tibble(
         {{ variable }} := 
           c(
-            population_params %>% names,
+            population_params %>% names(),
             'MISSING'
           ),
         prop = 
           c(
             population_params %>% unname() * nrow(dataset),
-            (dataset %>% dplyr::filter({{ variable }} == 'MISSING')) %>% nrow()
+            (dataset %>% dplyr::filter({{ variable }} == 'MISSING') %>% nrow())
           ) 
+      )
+    
+    ## Make prop table without the row for MISSING levels
+  } else {
+    
+    # Throws error if variable has missing values
+    if (NA %in% var_levels) {
+      
+      stop(
+        stringr::str_c(
+          'variable ',
+          variable_string,
+          ' contains NA values; please set to "MISSING"'
+        )
+      )
+      
+    }
+    
+    # Table
+    prop_table <- 
+      tibble::tibble(
+        {{ variable }} := 
+          population_params %>% names(),
+        prop = 
+          population_params %>% unname() * nrow(dataset)
+      ) 
+    
+  }
+  
+  ## Ensure that weight groups strings are the same as the data
+  
+  if (haven::is.labelled(dataset[[variable_string]])) {
+    
+    # For haven labelled variables
+    expected_inputs <- 
+      dataset %>% 
+      y2clerk::freqs(
+        {{ variable }}
+      ) %>% 
+      dplyr::arrange(
+        .data$value
+      ) %>% 
+      dplyr::pull(
+        .data$value
       )
     
   } else {
     
-    ### Make the prop table when there are missing levels
-    prop_table <- 
-      tibble::tibble(
-        {{ variable }} := 
-          population_params %>% names,
-        prop = 
-          population_params %>% unname() * nrow(dataset)
-      ) 
-
-
-}
-  
-  expected_inputs <- 
-    dataset %>% 
-    y2clerk::freqs(
-      {{ variable }}
-    ) %>% 
-    dplyr::arrange(
-      .data$label
-    ) %>% 
-    dplyr::pull(
-      .data$label
+    # All else
+    expected_inputs <- 
+      dataset %>% 
+      y2clerk::freqs(
+        {{ variable }}
+      ) %>% 
+      dplyr::arrange(
+        .data$label
+      ) %>% 
+      dplyr::pull(
+        .data$label
       )
     
+  }
+  
+  ## Ensure inputs are correct
+  
   supplied_inputs <- 
     prop_table %>% 
     dplyr::arrange(
@@ -100,9 +155,9 @@ create_prop_table_y2 <- function(
   
   only_in_supplied <- 
     supplied_inputs %>% 
-      setdiff(
-        expected_inputs
-      )
+    setdiff(
+      expected_inputs
+    )
   
   only_in_expected <- 
     expected_inputs %>% 
@@ -110,140 +165,61 @@ create_prop_table_y2 <- function(
       supplied_inputs
     )
   
-  ### Ensure that weight groups strings are the same as the data
-  if(  
+  if (length(only_in_expected) > 0) {
     
-    only_in_supplied %>% 
-    length() > 0 |
-      only_in_expected %>% 
-      length() > 0
-    
-  ){
-    
-    message(
-        'Warning: Expected inputs do not match the supplied inputs.'
-        )
-    
-    if(only_in_expected %>% 
-       length() > 0){
-      
-      message(
-        stringr::str_c(
-          'Expected inputs [ ',
-          stringr::str_flatten(
-            only_in_expected, 
-            collapse = ', '
-            ),
-          ' ] are not found in supplied inputs.'
-        )
+    stop(
+      stringr::str_c(
+        'Expected inputs [',
+        stringr::str_flatten(
+          only_in_expected, 
+          collapse = ', '
+        ),
+        '] are not found in supplied inputs.'
       )
-      
-    }
-    
-    if(only_in_supplied %>% 
-       length() > 0){
-      
-      message(
-        stringr::str_c(
-          'Supplied inputs [ ',
-          stringr::str_flatten(
-            only_in_supplied, 
-            collapse = ', '
-            ),
-          ' ] are not found in expected inputs.'
-        )
-      )
-      
-    }
+    )
     
   }
   
-  ### Check if the population_params add up to 100%
-  if(  
+  if (length(only_in_supplied) > 0) {
     
-    sum(population_params) %>% round(2) != 1
-    
-  ){
-    
-    message(
+    stop(
       stringr::str_c(
-        'Warning: Provided weighting proportions != 1. ',
-        'The sum of the supplied proportions == ',
+        'Supplied inputs [',
+        stringr::str_flatten(
+          only_in_supplied, 
+          collapse = ', '
+        ),
+        '] are not found in expected inputs.'
+      )
+    )
+    
+  }
+  
+  ## Check if the population_params add up to 100%
+  if (sum(population_params) != 1) {
+    
+    warning(
+      stringr::str_c(
+        'Provided total weighting proportions is not 1. ',
+        'The sum of the supplied proportions is ',
         sum(population_params)
-        ) 
-      )
-    
-  }
-  
-  
-  ### Check to see if add_missing_row should be set to TRUE
-  n <- 
-    dataset %>% 
-    y2clerk::freqs(
-      {{ variable }}
-    ) %>% 
-    dplyr::filter(
-      .data$label == 'MISSING'
-    ) %>% 
-    dplyr::count() %>% 
-    dplyr::pull(
-      n
-    )
-  
-  if(  
-    
-    n > 0 & add_missing_row == FALSE
-    
-  ){
-    
-    message(
-      stringr::str_c(
-        'Warning: add_missing_row = FALSE, but "MISSING" levels detected in data column ',
-        variable_string
       )
     )
     
   }
   
+  ## Send to environment
   
-  ### Check to see if there are NA's in the column
-  n <- 
-    dataset %>% 
-    y2clerk::freqs(
-      {{ variable }}
-    ) %>% 
-    dplyr::filter(
-      is.na(.data$label)
-    ) %>% 
-    dplyr::count() %>% 
-    dplyr::pull(.data$n)
-  
-  if(  
-    
-    n > 0 
-    
-  ){
-    
-    message(
-      stringr::str_c(
-        'Warning: NAs detected in data column ',
-        variable_string,
-        '. Please fix this before continuing.'
-      )
+  prop_table_name <- 
+    stringr::str_c(
+      variable_string,
+      '_prop_table'
     )
-    
-  }
   
-  
-  ### Send the prop table to the environment
-  prop_table %>%
-    tibble::tibble() %>% 
-    list() %>%
-    purrr::set_names(
-      pps_string
-    ) %>% 
-    list2env(envir = .GlobalEnv) %>%
-    invisible()
+  assign(
+    x = prop_table_name,
+    value = prop_table,
+    envir = .GlobalEnv
+  )
   
 }
-
