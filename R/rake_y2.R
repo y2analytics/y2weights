b213cde2df762046be28f741555460d24310c2b7
Canyen Heimuli <canyenheimuli@gmail.com>
2023-06-21 10:30:46 -0400

    Updates for initial release

--- R/rake_y2.R
+++ R/rake_y2.R
@@ -5,10 +5,10 @@
 #' Use rake_y2() in conjunction with create_prop_table_y2() to rake weights cleanly and with easier to understand errors. rake_y2() will automatically pull population parameter tables
 #'
 #' @keywords freqs weights population parameters
-#' @param dataset A dataframe to be weighted
+#' @param dataset A dataframe to be used in creating the survey design.
 #' @param ... List of variables 
 #' @export
-#' @return A complex survey object
+#' @return A complex raked survey design object.
 #' @examples
 #' \dontrun{
 #' survey::svydesign(
@@ -18,23 +18,21 @@
 #'}
 
 rake_y2 <- function(
-  dataset,
-  ...
+    dataset,
+    ...
 ){
-
-  svy_start_weight <-
-    survey::svydesign(
-      ids = ~1, 
-      data = dataset
-    )
-
+  
+  # Get variable string
+  
   variable_string <-
     dataset %>%
     dplyr::select(
       ...
     ) %>%
-    names
-
+    names()
+  
+  # Prep variable names for function
+  
   variable_list <-
     stringr::str_c(
       '~',
@@ -43,30 +41,81 @@ rake_y2 <- function(
     purrr::map(
       ~as.formula(.x)
     )
-
+  
+  # Ensure every prop_table exists
+  
   props_list <-
     stringr::str_c(
       variable_string,
       '_prop_table'
-      ) %>%
+    )
+  
+  nonexistent <- c()
+  
+  for (prop in props_list) {
+    
+    if (!exists(prop)) {
+      
+      nonexistent <- append(nonexistent, prop)
+      
+    }
+    
+  }
+  
+  if (length(nonexistent > 0)) {
+    
+    stop(
+      stringr::str_c(
+        'Expected prop_table(s) "',
+        paste(
+          nonexistent,
+          collapse = ', '
+        ),
+        '" not found'
+      )
+    )
+    
+  }
+  
+  # Prep prop table list for function
+  
+  props_list <-
+    stringr::str_c(
+      variable_string,
+      '_prop_table'
+    ) %>%
     purrr::map(
       ~eval(
         as.symbol(
           .x
-          )
         )
+      )
     )
-
-  ss_rake <- 
+  
+  # Initialize
+  
+  svy_start_weight <-
+    survey::svydesign(
+      ids = ~1,
+      weights = NULL,
+      data = dataset
+    )
+  
+  # Create survey design
+  
+  svy_design <- 
     survey::rake(
-    design =
-      svy_start_weight,
-    sample.margins =
-      variable_list,
-    population.margins =
-      props_list
-  )
-
-  ss_rake
-
+      design =
+        svy_start_weight,
+      sample.margins =
+        variable_list,
+      population.margins =
+        props_list
+    ) %>% 
+    suppressWarnings()
+  
+  # Return
+  
+  return(svy_design)
+  
 }
